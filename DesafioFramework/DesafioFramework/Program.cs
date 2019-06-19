6fccab5d8721e16f82955b6e681b0e97d2e5b47b
daran10 <36827107+daran10@users.noreply.github.com>
2019-06-19 18:57:45 -0300

    Add files via upload

---
+++ DesafioFramework/DesafioFramework/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace DesafioFramework
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int i, j, k, l, n;
+            Console.Write("digite um número para formar a pirâmide:   ");
+            n = Convert.ToInt32(Console.ReadLine());
+            for (i = 1; i <=n; i++)
+            {
+                    for (j = n - i; j>0; j--)
+                   {
+                       Console.Write("    ");
+                   }
+
+                {
+                    for (k = 1; k < i; k++)
+                    {
+                        Console.Write("framework", k);
+                    }
+                    for(l = i; l > 0; l--)
+                    {
+                        Console.Write(" ", l);
+                    }
+                
+                    Console.WriteLine();
+                }
+                    
+            }
+        }
+    }
+}
